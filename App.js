895f3cacf4b67962a76752cb54bf7a2ffc572123
João <joao.codelearning@gmail.com>
2025-10-08 22:28:18 -0300

    estilização da StatusBar através da SafeAreaView

--- App.js
+++ App.js
@@ -2,13 +2,12 @@ import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 import { ThemeProvider } from "./src/contexts/theme/ThemeContext";
 import { ThemeSwitcher } from "./src/contexts/theme/ThemeSwitcher";
 import { ClientsProvider } from "./src/contexts/clients/ClientsContext";
-import { PaperProvider } from "react-native-paper";
 import { Routes } from "./src/routes";
 export default function App() {
   return (
     <ThemeProvider>
       <SafeAreaProvider>
-        <SafeAreaView style={{ flex: 1 }}>
+        <SafeAreaView style={{ flex: 1, backgroundColor:'#121212' }}>
           <ClientsProvider>
               <Routes />
           </ClientsProvider>
