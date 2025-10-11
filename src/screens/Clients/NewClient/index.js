09e09150486b437fc1855cf5324a5a8597c588bf
João <joao.codelearning@gmail.com>
2025-10-10 22:41:42 -0300

    estiliza tela de cadastro de novo cliente

--- src/screens/Clients/NewClient/index.js
+++ src/screens/Clients/NewClient/index.js
@@ -1,8 +1,16 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import { useTheme } from "../../../contexts/theme/ThemeContext";
 import { useClients } from "../../../contexts/clients/ClientsContext";
+import { Input } from "../../../components/Input";
+import { Button } from "../../../components/Button";
+import { useState } from "react";
+import { style } from "./style";
 
 export function NewClient({ route }) {
+  const [clientName, setClientName] = useState("");
+  const [cnpj, setCnpj] = useState("");
+  const [clientContact, setClientContact] = useState("");
+
   const { clients } = useClients();
   const { theme } = useTheme();
 
@@ -18,9 +26,34 @@ export function NewClient({ route }) {
         },
       ]}
     >
-      <Text style={{ color: theme.text, fontSize: 24, fontWeight: "bold" }}>
-        New Client
-      </Text>
+      <Input
+        onChangeText={setClientName}
+        value={clientName}
+        placeholder={"Nome do cliente"}
+        color={theme.text}
+        placeHolderColor={theme.text}
+      />
+      <Input
+        onChangeText={setCnpj}
+        value={cnpj}
+        placeholder={"CNPJ"}
+        color={theme.text}
+        placeHolderColor={theme.text}
+        type={"numeric"}
+      />
+      <Input
+        onChangeText={setClientContact}
+        value={clientContact}
+        placeholder={"Email"}
+        color={theme.text}
+        placeHolderColor={theme.text}
+        type={"email"}
+      />
+      <Button
+        btnText={"Adicionar Cliente"}
+        style={[style.btn, { backgroundColor: theme.primary }]}
+        textStyle={style.btnText}
+      />
     </View>
   );
 }
